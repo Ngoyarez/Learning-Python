a10a1091222228ea96cc32d0633928e9992e6c89
Ngoyarez <brianngoya764@yahoo.com>
2024-01-08 00:08:46 +0300

    F Challenge

---
+++ For Loops/f_challenge.py
@@ -0,0 +1,20 @@
+# numbers = [5, 2, 5, 2, 2]
+# for number in numbers:
+#     print("X " * number)
+
+
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'X '
+    print(output)
+
+
+
+numbers = [2, 2, 2, 2]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'X '
+    print(output)
