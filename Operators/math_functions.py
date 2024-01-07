ae50d48d57c9d44341235e3e2682169ee56a124a
Ngoyarez <brianngoya764@yahoo.com>
2024-01-07 19:29:35 +0300

    Math Functions

---
+++ Operators/math_functions.py
@@ -0,0 +1,9 @@
+x = 2.9
+x1 = -2.9
+print(round(x)) # Rounds the number to whole number
+print(abs(x1)) # Returns the positive representation of a number
+# To use math functions, you need to import a math module which contains a bunch of reusable math functions
+import math
+print(math.ceil(2.9))
+print(math.floor(2.9))
+# To learn about these math modules on your own, search for python 3 math modules
