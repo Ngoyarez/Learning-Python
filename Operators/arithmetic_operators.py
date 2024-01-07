e92366307cf4959aa61a2786888022887489f4b6
Ngoyarez <brianngoya764@yahoo.com>
2024-01-07 19:26:41 +0300

    Arithmetic Operators

---
+++ Operators/arithmetic_operators.py
@@ -0,0 +1,20 @@
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+
+#Augumented assignment operator
+x = 10
+x = x + 3
+# x += 3
+# x -= 3
+# x *= 3
+# x /= 3
+
+# x = 10 + 3 * 2 = 16
+# Order of precedence - parentheses, exponentiation, multiplication or division, addition or subtraction
+
+print(x)
