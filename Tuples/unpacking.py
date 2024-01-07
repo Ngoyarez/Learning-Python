563413df751c029a0c02f89c343ecd87549bb197
Ngoyarez <brianngoya764@yahoo.com>
2024-01-07 19:06:25 +0300

    Unpacking Method

---
+++ Tuples/unpacking.py
@@ -0,0 +1,10 @@
+coordinates = (1, 2, 3)
+# coordinates[0] * coordinates[1] * coordinates[2]
+x = coordinates[0]
+y = coordinates[1]
+z = coordinates[2]
+
+# x * y * z
+
+x, y, z = coordinates # The python interpreter will get the first element of the tuple and assign it to x
+print(x)
