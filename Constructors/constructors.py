c3efbfc37f99a53b86954de9775db546cadce1f8
Ngoyarez <brianngoya764@yahoo.com>
2024-02-05 19:38:15 +0300

    Python Constructors

---
+++ Constructors/constructors.py
@@ -0,0 +1,16 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+
+point = Point(10, 20)
+point.x = 11
+print(point.x)
+
