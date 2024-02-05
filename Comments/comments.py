7a7fd5551868920a689bf69f7a812b8b390992a2
Ngoyarez <brianngoya764@yahoo.com>
2024-02-05 19:23:17 +0300

    Comments in Python

---
+++ Comments/comments.py
@@ -0,0 +1,10 @@
+# Brian Ngoya
+print("Sky is blue")
+# Comments can be used to talk to other developers about the program
+# It can also be used to improve the readability of the code
+# You should not explain what the code does
+
+
+# BAD COMMENT EXAMPLE
+# print Sky is blue
+# print("Sky is blue")
